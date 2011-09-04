f6bb5f77eff2bc06826ff82181c7b6e53b088cb0
Vlad Tudose <vlad_tudose@yahoo.com>
2011-09-04 19:11:00 +0300

    added cradle

---
+++ 1/cradle.h
@@ -0,0 +1,25 @@
+#ifndef _CRADLE_H
+#define _CRADLE_H
+#define UPCASE(C) ((1<<6)| (C))
+
+char Look;
+
+void GetChar();
+
+void Error(char *s);
+void Abort(char *s);
+void Expected(char *s);
+void Match(char x);
+
+int IsAlpha(char c);
+int IsDigit(char c);
+
+char GetName();
+char GetNum();
+
+void Emit(char *s);
+void EmitLn(char *s);
+
+void Init();
+
+#endif
